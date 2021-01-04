70bc345630801592262b208ecb0b80a3771d2c2c
keerthanak-tw <76804118+keerthanak-tw@users.noreply.github.com>
2021-01-04 15:43:19 +0530

    Update PrimeFactors.java

--- src/main/java/simplest/possible/implementation/PrimeFactors.java
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -9,16 +9,7 @@ public class PrimeFactors {
         if(number == 1)
             return factors;
         else
-            if(isPrime(number)){
-                factors.add(number);
-            }
+            factors.add(number);
         return factors;
-    }
-
-    private boolean isPrime(int number) {
-        for(int divisor = 2; divisor < number/2; divisor++)
-            if(number%divisor == 0)
-                return false;
-        return true;
     }
 }
